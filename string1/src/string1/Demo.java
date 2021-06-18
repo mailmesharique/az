96f06894e30a53ac4130ae6a674d79e1417a50a9
mailmesharique <mailmesharique@gmail.com>
2021-06-19 01:05:58 +0530

    First Commit

---
+++ string1/src/string1/Demo.java
@@ -0,0 +1,23 @@
+
+
+package string1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String str="raman";
+		String str2="raman";
+		
+		String str1= new String("ratan");
+		String str3= new String("ratan");
+		System.out.println(str);
+		System.out.println(str1);
+		System.out.println(str==str2);
+		System.out.println(str1==str3);
+		
+		
+		
+
+	}
+
+}
